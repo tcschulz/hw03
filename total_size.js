14e309e4a466e1fa48595122247f2af195d413ec
tcschulz <tanya.schulz@colorado.edu>
2018-10-09 15:34:12 -0600

    second draft

--- total_size.js
+++ total_size.js
@@ -1,53 +1,64 @@
-// CSCI 5828: Spring 2018
-// Homework 3
+// CSCI 5828: Spring 2018, HW 3
 // Tanya Schulz
-// September 30, 2018
+// October 9, 2018
 
-//Doesn't function properly yet... 
-
-
+//Still doesn't function properly but works for empty directories now...
 
 var fs = require('fs'); //file system module 
 var args = process.argv; //command line arguments
-
-console.log(`Starting directory: ${process.cwd()}`);
+var recursive = require("recursive-readdir");
 
 process.chdir(args[2]);
-console.log(`New directory: ${process.cwd()}`);
+console.log(`Directory: ${process.cwd()}`);
 
-var all_done = function(size) {
+var all_done = function(size) { //executes first
 	console.log("Total size:", size);
 }
 
-var handleFile = function(stats, i, filenames, total) {
-	if (i === filenames.length - 1) {
-		all_done(total + stats.size);
-	} 	else {
-		processFile(i+1, filenames, total+stats.size);
-	}
-}
+recursive(process.cwd(), function (err, files) { //array of all file paths
+  if (files.length == 0) { 
+	console.log("Total size: 0")
+	//end if directory and/or subdirectories are all empty
+  }	else {
+	  console.log("All files: ");
+	  console.log(files);
+	  for (i in files) {
 
-var handleDir = function(i, filenames, total) {
-	if (i=== filenames.length - 1) {
-		all_done(total);
-	}	else {
-	processFile(i+1, filenames, total);
-	}
-}
-
-var processFile = function(i, filenames, total) {
-	var name = filenames[i];
-	fs.stat(name, function(err, stats) {
-		if (err) throw err;
-		if (stats.isFile()) {
-			handleFile(stats, i, filenames, total);
-		} else {
-		  handleDir(i, filenames, total);
+		var handleFile = function(stats, i, filenames, total) {
+			if (i === filenames.length - 1) {
+				all_done(total + stats.size);
+			} 	else {
+				processFile(i+1, filenames, total+stats.size);
+			}
+		}
+		
+		var handleDir = function(i, filenames, total) {
+			if (i=== filenames.length - 1) {
+				all_done(total);
+			}	else {
+			processFile(i+1, filenames, total);
+			}
+		}
+		
+		var processFile = function(i, filenames, total) {
+			var name = filenames[i];
+			fs.stat(name, function(err, stats) {
+				if (err) throw err;
+				if (stats.isFile()) {
+					handleFile(stats, i, filenames, total);
+				} else {
+				  handleDir(i, filenames, total);
+			  }
+		  });
+		}
+		
+		fs.readdir('.', function(err, filenames) {
+			if (err) throw err;
+			processFile(0, filenames, 0);
+		});
 	  }
-  });
-}
-
-
+	}
+});
 
 
 
@@ -61,5 +72,6 @@ var processFile = function(i, filenames, total) {
 // References:
 // 1. https://www.npmjs.com/package/prompt-async
 // 2. https://nodejs.org/api/process.html
+// 3. https://www.npmjs.com/package/recursive-readdir
 
 
